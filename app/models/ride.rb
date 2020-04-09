98575f89bb1bacc6e6b71b7c4e0e8abb9e1db8ee
Elana Ryznar <eryzna@gmail.com>
2020-04-09 20:54:47 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -28,11 +28,11 @@ class Ride < ActiveRecord::Base
 
   def start_ride
     new_happiness = self.user.happiness + self.attraction.happiness_rating
-  #  new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
     new_ticket_count = self.user.tickets - self.attraction.tickets
     self.user.update(
       :happiness => new_happiness,
-      #:nausea => new_nausea,
+      :nausea => new_nausea,
       :tickets => new_ticket_count
     )
     "Thanks for riding the #{self.attraction.name}!"
